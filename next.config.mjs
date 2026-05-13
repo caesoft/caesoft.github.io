c23b4a3138c211fc12160604801f58f1680c9a19
Luis Felipe <mendesmel420@gmail.com>
2026-05-13 20:18:09 -0300

    Venci

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
-  basePath: '',
-  assetPrefix: '',
-  trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
